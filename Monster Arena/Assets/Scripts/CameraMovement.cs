4fab4a8bf0410c15426a8a288c1f2f5e5c7cad64
linnos <39270663+linnos@users.noreply.github.com>
2025-02-02 15:59:12 -0500

    Updated camera movement
    Fixed jitter and made it to where you can move the camera by joystick and mouse. Mouse needed a clamp since it was going too fast. Camera speed is controlled by a camera speed variable.

--- Monster Arena/Assets/Scripts/CameraMovement.cs
+++ Monster Arena/Assets/Scripts/CameraMovement.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -14,14 +15,19 @@ public class CameraMovement : MonoBehaviour
     private float _rotationXAxis;
     private float _rotationYAxis;
 
-    private void Update()
+    Vector2 input;
+    public Vector3 directions = Vector3.zero;
+
+    public float cameraSpeed;
+
+    private void LateUpdate()
     {
 
-        Vector3 direction = previousPosition - camera.ScreenToViewportPoint(Input.mousePosition);
+        Vector3 direction = previousPosition - camera.transform.position - directions;
 
         camera.transform.position = target.position;
         //Rotate around x axis. Vertical.
-        _rotationXAxis = Mathf.Clamp(_rotationXAxis + direction.y * 180, -15, 45);
+        _rotationXAxis = Mathf.Clamp(_rotationXAxis + direction.y * cameraSpeed, -15, 45);
 
         //This does a cool tilt effect like from an FPS. Leaving here just in case.
         // transform.rotation = Quaternion.Euler(0, 0, _rotation);
@@ -30,15 +36,36 @@ public class CameraMovement : MonoBehaviour
         // transform.rotation = Quaternion.Euler(_rotation, 0, 0);
         
         //Rotate around y axis. Horizontal
-        _rotationYAxis = _rotationYAxis + -direction.x * 180;
+        _rotationYAxis = _rotationYAxis + -direction.x * cameraSpeed;
         
-        transform.rotation = Quaternion.Euler(_rotationXAxis, _rotationYAxis, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(_rotationXAxis, _rotationYAxis, 0), cameraSpeed);
+        // Quaternion.Euler(_rotationXAxis, _rotationYAxis, 0);
             // camera.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
 
         // camera.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 180, Space.World);
         camera.transform.Translate(new Vector3(adjustX, adjustY, adjustZ));
 
-        previousPosition = camera.ScreenToViewportPoint(Input.mousePosition);
+        previousPosition = camera.transform.position;
+
+    }
+
+    public void Look_Event(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+           
+        }
+        else if (context.performed)
+        {
+            input = context.ReadValue<Vector2>();
+            directions.x = Mathf.Clamp(input.x, -1f, 1);
+            directions.y = Mathf.Clamp(input.y, -1f, 1);
+        }
+        else if (context.canceled)
+        {
+            directions.x = 0f;
+            directions.y = 0f;
+        }
 
     }
 }
