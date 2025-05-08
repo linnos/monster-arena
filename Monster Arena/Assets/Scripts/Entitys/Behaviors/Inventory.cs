b4ce85de8ad686e0a0aa9fe11e4d8be9e6c45338
linnos <39270663+linnos@users.noreply.github.com>
2025-05-08 18:52:21 -0400

    Potion ui added

--- Monster Arena/Assets/Scripts/Entitys/Behaviors/Inventory.cs
+++ Monster Arena/Assets/Scripts/Entitys/Behaviors/Inventory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 //A basic inventory system for a health potion.
@@ -9,12 +10,29 @@ public class Inventory : MonoBehaviour
     //it's an action that takes an int because thats houw much health the potion gives.
     public Action<int> OnItemUsed;
     public int numOfPotions = 3;
+    
+    //TODO: Remove this temporary potion code
+    //This is just for testing purposes. Remove this when you have a proper item system.
+    public List<GameObject> potionUIPrefab;
 
+    private void Awake()
+    {
+        if(potionUIPrefab == null)
+        {
+            throw new Exception("Potion UI prefab is not assigned in the inspector.");
+        }
+        if(potionUIPrefab.Count == 0)
+        {
+            throw new Exception("Potion UI prefab list is empty in the inspector.");
+        }
+    }
+    
     //Prepares the item for use. When the button is pressed, it will check if the item is available.
     //If it is, it will decrement the number of potions and return true.
     public bool PrepareItem(){
         if(numOfPotions > 0){
             numOfPotions--;
+            potionUIPrefab[numOfPotions].SetActive(false);
             return true;
         }
 
